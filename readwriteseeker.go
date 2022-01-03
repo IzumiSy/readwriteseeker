86dec8bf6e66c82c2efff8bfdddb1dc4a19c8715
IzumiSy <beetle-noise@gmx.com>
2022-01-03 16:02:34 +0900

    Fix

--- readwriteseeker.go
+++ readwriteseeker.go
@@ -11,32 +11,32 @@ var _ io.ReadWriteSeeker = &ReadWriteSeeker{}
 
 type ReadWriteSeeker struct {
 	sync.Mutex
-	rs *writerseeker.WriterSeeker
+	ws *writerseeker.WriterSeeker
 }
 
 func New() *ReadWriteSeeker {
 	return &ReadWriteSeeker{
-		rs: &writerseeker.WriterSeeker{},
+		ws: &writerseeker.WriterSeeker{},
 	}
 }
 
 func (rs *ReadWriteSeeker) Write(p []byte) (n int, err error) {
 	rs.Lock()
 	defer rs.Unlock()
-	n, err = rs.Write(p)
+	n, err = rs.ws.Write(p)
 	return
 }
 
 func (rs *ReadWriteSeeker) Seek(offset int64, whence int) (n int64, err error) {
 	rs.Lock()
 	defer rs.Unlock()
-	n, err = rs.Seek(offset, whence)
+	n, err = rs.ws.Seek(offset, whence)
 	return
 }
 
 func (rs *ReadWriteSeeker) Read(p []byte) (n int, err error) {
 	rs.Lock()
 	defer rs.Unlock()
-	n, err = rs.rs.Reader().Read(p)
+	n, err = rs.ws.Reader().Read(p)
 	return
 }
